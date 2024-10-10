f7e92fce20638681d4816087561a6837bb0c51e1
MartinLocker <37294080+MartinLocker@users.noreply.github.com>
2024-10-10 13:47:29 +0200

    Update main.ts, sensors.ts

--- main.ts
+++ main.ts
@@ -173,69 +173,6 @@ namespace Drive {
     }
 }
 
-/**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org/blocks/custom
- */
-enum Distance_Unit {
-    //% block="mm" enumval=0
-    Distance_Unit_mm,
-
-    //% block="cm" enumval=1
-    Distance_Unit_cm,
-
-    //% block="inch" enumval=2
-    Distance_Unit_inch,
-}
-
-/**
- * Custom blocks
- */
-//% color=#0fbc11 icon="\uf140"
-namespace Sonar {
-
-    /**
-    * get Ultrasonic distance
-    */
-    //% blockId=sonarbit block="Ultrasonic distance in unit %distance_unit |at|pin %pin"
-    //% weight=10
-    export function sonarbit_distance(distance_unit: Distance_Unit, pin: DigitalPin): number {
-
-        let old = 0
-        let now = 0
-        // send pulse
-        pins.setPull(pin, PinPullMode.PullNone)
-        pins.digitalWritePin(pin, 0)
-        control.waitMicros(2)
-        pins.digitalWritePin(pin, 1)
-        control.waitMicros(10)
-        pins.digitalWritePin(pin, 0)
-
-        // read pulse
-        let d = pins.pulseIn(pin, PulseValue.High, 25000)  // 8 / 340 = 
-
-        let distance = d / 58
-
-        if (distance > 400) {
-            distance = 0
-        }
-
-        switch (distance_unit) {
-            case 0:
-                return Math.floor(distance * 10) //mm
-                break
-            case 1:
-                return Math.floor(distance)  //cm
-                break
-            case 2:
-                return Math.floor(distance / 254)   //inch
-                break
-            default:
-                return 0
-        }
-
-    }
-}
 
 /**
 * Functions to WuKong multifunctional expansion board by ELECFREAKS Co.,Ltd.
