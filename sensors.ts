e6872485e40a959092b9630c56eced16bd609d1f
MartinLocker <37294080+MartinLocker@users.noreply.github.com>
2024-10-10 14:23:57 +0200

    Update sensors.ts

--- sensors.ts
+++ sensors.ts
@@ -1,6 +1,58 @@
 //% weight=100 color=#000000 icon="\uf043" block="Sensors"
 namespace Sensors {
 
+    enum Distance_Unit {
+        //% block="mm" enumval=0
+        Distance_Unit_mm,
+
+        //% block="cm" enumval=1
+        Distance_Unit_cm,
+
+        //% block="inch" enumval=2
+        Distance_Unit_inch,
+    }
+
+    /**
+    * get Ultrasonic distance
+    */
+    //% blockId=sonarbit block="Ultrasonic distance in unit %distance_unit |at|pin %pin"
+    //% weight=10
+    //% subcategory="Sonar"
+    export function sonarbit_distance(distance_unit: Distance_Unit, pin: DigitalPin): number {
+
+        // send pulse
+        pins.setPull(pin, PinPullMode.PullNone)
+        pins.digitalWritePin(pin, 0)
+        control.waitMicros(2)
+        pins.digitalWritePin(pin, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(pin, 0)
+
+        // read pulse
+        let d = pins.pulseIn(pin, PulseValue.High, 25000)  // 8 / 340 = 
+
+        let distance = d / 58
+
+        if (distance > 400) {
+            distance = 0
+        }
+
+        switch (distance_unit) {
+            case 0:
+                return Math.floor(distance * 10) //mm
+                break
+            case 1:
+                return Math.floor(distance)  //cm
+                break
+            case 2:
+                return Math.floor(distance / 254)   //inch
+                break
+            default:
+                return 0
+        }
+
+    }
+
     class tcs3472 {
         is_setup: boolean
         addr: number
@@ -145,57 +197,6 @@ namespace Sensors {
         return _tcs3472.setIntegrationTime(time)
     }
 
-    enum Distance_Unit {
-        //% block="mm" enumval=0
-        Distance_Unit_mm,
-
-        //% block="cm" enumval=1
-        Distance_Unit_cm,
-
-        //% block="inch" enumval=2
-        Distance_Unit_inch,
-    }
-
-    /**
-    * get Ultrasonic distance
-    */
-    //% blockId=sonarbit block="Ultrasonic distance in unit %distance_unit |at|pin %pin"
-    //% weight=10
-    //% subcategory="Sonar"
-    export function sonarbit_distance(distance_unit: Distance_Unit, pin: DigitalPin): number {
-
-        // send pulse
-        pins.setPull(pin, PinPullMode.PullNone)
-        pins.digitalWritePin(pin, 0)
-        control.waitMicros(2)
-        pins.digitalWritePin(pin, 1)
-        control.waitMicros(10)
-        pins.digitalWritePin(pin, 0)
-
-        // read pulse
-        let d = pins.pulseIn(pin, PulseValue.High, 25000)  // 8 / 340 = 
-
-        let distance = d / 58
-
-        if (distance > 400) {
-            distance = 0
-        }
-
-        switch (distance_unit) {
-            case 0:
-                return Math.floor(distance * 10) //mm
-                break
-            case 1:
-                return Math.floor(distance)  //cm
-                break
-            case 2:
-                return Math.floor(distance / 254)   //inch
-                break
-            default:
-                return 0
-        }
-
-    }
 }
 
 
